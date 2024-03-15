d77158bd21b653f4ac3f0af2e3d15bad6c19ce92
Arseni-code <86353560+Arseni-code@users.noreply.github.com>
2024-03-15 22:32:07 +0700

    Пофиксил баги.

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ import os
 import shutil
 import sys
 
-PACKAGES_DIR = "/home/user/packages"  # Путь к директории с пакетами
-INSTALLATION_DIR = "/home/user/package_manager"  # Путь к директории для установки пакетов
+PACKAGES_DIR = "/home/arseni/a!package_manager/packages"  # Путь к директории с пакетами
+INSTALLATION_DIR = "/home/arseni/a!package_manager/package_placement"  # Путь к директории для установки пакетов
 
 def install(package_name):
     package_path = os.path.join(PACKAGES_DIR, package_name)
     if os.path.exists(package_path):
         try:
             shutil.copytree(package_path, os.path.join(INSTALLATION_DIR, package_name))
-            print(f"Package '{package_name}' has been installed successfully!")
+            print(f"Package '{package_name}' has installed successfully!")
+        except FileExistsError:
+            print(f"Package '{package_name}' has already installed!")
         except Exception as e:
             print(f"Failed to install package '{package_name}': {str(e)}")
     else:
@@ -21,7 +23,7 @@ def uninstall(package_name):
     if os.path.exists(package_path):
         try:
             shutil.rmtree(package_path)
-            print(f"Package '{package_name}' has been uninstalled successfully!")
+            print(f"Package '{package_name}' has uninstalled successfully!")
         except Exception as e:
             print(f"Failed to uninstall package '{package_name}': {str(e)}")
     else:
@@ -31,22 +33,21 @@ def list_installed_packages():
     packages = [name for name in os.listdir(INSTALLATION_DIR) if os.path.isdir(os.path.join(INSTALLATION_DIR, name))]
     print("Installed packages:")
     for package in packages:
-        print(f"- {package}")
+        print(package)
 
 # Обработка команд из командной строки
 if __name__ == "__main__":
-    command = sys.argv[1] if len(sys.argv) > 1 else None
-    package_name = sys.argv[2] if len(sys.argv) > 2 else None
+    command = input()
+    command = command.split()
 
-    if command == "install" and package_name:
-        install(package_name)
-    elif command == "uninstall" and package_name:
-        uninstall(package_name)
-    elif command == "list":
+    if command[0] == "install":
+        install(command[1])
+    elif command[0] == "uninstall":
+        uninstall(command[1])
+    elif command[0] == "list_installed_packages":
         list_installed_packages()
-    else:
+    elif command[0] == "help":
         print("Usage:")
         print("- To install a package: python package_manager.py install <package_name>")
         print("- To uninstall a package: python package_manager.py uninstall <package_name>")
         print("- To list installed packages: python package_manager.py list")
-
